98aee3368e69b9042eeb0a41a66f70453183430a
Oleksii Lysak <lysakop@gmail.com>
2021-10-14 18:57:12 +0300

    Add parse_categories function and the basis for parse_product_list.

--- audible/spiders/audible_co_uk_discovery.py
+++ audible/spiders/audible_co_uk_discovery.py
@@ -16,3 +16,20 @@ class AudibleCoUkDiscoverySpider(scrapy.Spider):
         for top_category in response.css('.bc-row-responsive.bc-spacing-medium h2 a'):
             yield scrapy.Request(url=urljoin(response.url, top_category.css('::attr(href)')),
                                  callback=self.parse_categories)
+
+    def parse_categories(self, response):
+        for category in response.css('.bc-row-responsive.subCategoriesContainer div'):
+            if category.css('a'):
+                yield scrapy.Request(url=urljoin(response.url, category.css('a::attr(href)').get()),
+                                     callback=self.parse_categories)
+            yield scrapy.Request(
+                url=urljoin(response.url, response.css('.bc-col-responsive.bc-text-right.bc-col-4 a::attr(href)')),
+                callback=self.parse_product_list)
+
+    def parse_product_list(self, response):  # NOQA
+        for product in response.css('div.adbl-impression-container li.bc-list-item.productListItem'):
+            item = {
+                'publication_date': product.css('li.bc-list-item.releaseDateLabel span::text').re_first(
+                    r'(\d{2}-\d{2}-\d{2})'),
+                'lang': product.css('li.bc-list-item.languageLabel span::text').re_first(r'(\w*)\s*$')
+            }
